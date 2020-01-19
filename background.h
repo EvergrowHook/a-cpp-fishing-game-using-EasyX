c52d93176aee328980fbb63c5e268153055cc134
Edward Hook <57997827+EdwardHook@users.noreply.github.com>
2020-01-19 23:05:52 +0800

    Delete background.h

--- background.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-void createbutton(int l, int r, int u, int d, int size, LPCTSTR lpszFace);
-void setheadline(int x, int y, int size, LPCTSTR lpszFace);
-void drawAlpha(IMAGE* picture, int  picture_x, int picture_y);
-void loadAlpha(IMAGE* img, LPCTSTR pImgFile, int nWidth, int nHeight, bool bResize);
-void transparentimage(IMAGE *dstimg, int x, int y, IMAGE *srcimg, UINT transparentcolor);
-void puttextxy(int x, int y, int num);
