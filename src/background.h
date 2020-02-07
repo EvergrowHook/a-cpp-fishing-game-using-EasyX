14f707a0fb9c68e1aa7c9926aa371462d5759dd9
Evergrow Hook <57997827+EvergrowHook@users.noreply.github.com>
2020-02-07 17:37:09 +0800

    rewrite annotations

--- src/background.h
+++ src/background.h
@@ -1,7 +1,12 @@
+/*
+ FILE: background.h/cpp
+ FUNCTION: some functions used for graphics
+ COPYRIGHT: Evergrow Hook
+*/
 #pragma once
-void createbutton(int l, int r, int u, int d, int size, LPCTSTR lpszFace);
-void setheadline(int x, int y, int size, LPCTSTR lpszFace);
-void drawAlpha(IMAGE* picture, int  picture_x, int picture_y);
-void loadAlpha(IMAGE* img, LPCTSTR pImgFile, int nWidth, int nHeight, bool bResize);
-void transparentimage(IMAGE *dstimg, int x, int y, IMAGE *srcimg, UINT transparentcolor);
-void puttextxy(int x, int y, int num);
+void createbutton(int l, int r, int u, int d, int size, LPCTSTR lpszFace);//create a button
+void setheadline(int x, int y, int size, LPCTSTR lpszFace);//create a headline
+void drawAlpha(IMAGE* picture, int  picture_x, int picture_y);// load a .png picture and remove the transparent part
+void loadAlpha(IMAGE* img, LPCTSTR pImgFile, int nWidth, int nHeight, bool bResize);//load an image in a clear way
+void transparentimage(IMAGE *dstimg, int x, int y, IMAGE *srcimg, UINT transparentcolor);//another way of showing a transparent image
+void puttextxy(int x, int y, int num);//show a number
