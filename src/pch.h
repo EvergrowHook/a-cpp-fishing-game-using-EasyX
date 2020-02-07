b935947a9d8a7ecf7d83af63d8be1656b50688e0
Evergrow Hook <57997827+EvergrowHook@users.noreply.github.com>
2020-02-07 16:52:58 +0800

    rewrite annotations

--- src/pch.h
+++ src/pch.h
@@ -1,7 +1,14 @@
-﻿#ifndef PCH_H
+/*
+ FILE: pch.h/cpp
+ FUNCTION: store the used headers in it(which is needed in visual studio)
+ COPYRIGHT: Evergrow Hook
+*/
+#ifndef PCH_H
 #define PCH_H
 
 // TODO: 添加要在此处预编译的标头
+
+//------------------headers-------------------
 #include <iostream>
 #include<Windows.h>
 #include<stdio.h>
@@ -18,15 +25,19 @@
 #include "background.h"
 #include "score.h"
 #include "achievement.h"
-#define t 1.5
-#define PI 3.14
-#define rad *PI/180
+
+//----------------definitions------------------
+#define t 1.5//proportion of velocity
+#define PI 3.14//value of pi
+#define rad *PI/180//convert from degree to rad
 #define CANVAS_WIDTH 480
 #define CANVAS_LENGTH 640//size of the canvas
-#define SCORE_INIT 50
-#define KIND 5
+#define SCORE_INIT 50//the initial score
+#define KIND 5//how many kinds of fish
+
+//---------------other orders------------------
 using namespace std;
 #pragma comment( lib, "MSIMG32.LIB")
-#pragma comment(lib,"winmm.lib")
+#pragma comment(lib,"winmm.lib")//use this for the playsound function
 
 #endif //PCH_H
