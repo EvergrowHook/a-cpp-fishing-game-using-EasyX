96d5084591cfbb4ee9d8a714ea9b28f8664de2b1
Evergrow Hook <57997827+EvergrowHook@users.noreply.github.com>
2020-02-07 17:40:00 +0800

    rewrite annotations

--- src/score.cpp
+++ src/score.cpp
@@ -1,14 +1,21 @@
+/*
+ FILE: score.h/cpp
+ FUNCTION: create and update the score board
+ COPYRIGHT: Evergrow Hook
+*/
 #include "pch.h"
 
+//init the score board
 void score::init() {
 	num = SCORE_INIT;
 }
 
+//update and show the score board
 void score::show() {
-	createbutton(578, 622, 18, 32, 10, _T("得分"));
+	createbutton(578, 622, 18, 32, 10, _T("碌脙路脰"));
 	createbutton(578, 622, 38, 52, 10, _T(""));
 	puttextxy(580, 40, num);
-	settextstyle(20, 0, _T("黑体"));
+	settextstyle(20, 0, _T("潞脷脤氓"));
 	settextcolor(YELLOW);
 	if (numtmp >= 0) {
 		outtextxy(570, 60, _T("+"));
