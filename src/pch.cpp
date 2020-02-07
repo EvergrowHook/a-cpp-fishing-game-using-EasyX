0d41f1c0367512c33cd4985f674bd3095b16e7b5
Evergrow Hook <57997827+EvergrowHook@users.noreply.github.com>
2020-02-07 16:44:13 +0800

    rewrite annotations

--- src/pch.cpp
+++ src/pch.cpp
@@ -1,4 +1,9 @@
-﻿// pch.cpp: 与预编译标头对应的源文件；编译成功所必需的
+/*
+ FILE: pch.h/cpp
+ FUNCTION: store the used headers in it(which is needed in visual studio)
+ COPYRIGHT: Evergrow Hook
+*/
+// pch.cpp: 与预编译标头对应的源文件；编译成功所必需的
 
 #include "pch.h"
 
