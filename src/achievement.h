b3bdea8a7da95ec97fb224ef85f62b49aa0683ed
Evergrow Hook <57997827+EvergrowHook@users.noreply.github.com>
2020-02-07 17:01:16 +0800

    rewrite annotations

--- src/achievement.h
+++ src/achievement.h
@@ -1,11 +1,17 @@
+/*
+ FILE: achievement.h/cpp
+ FUNCTION: create a fish-collection system
+ COPYRIGHT: Evergrow Hook
+*/
 #pragma once
 class fish_collect {
-public:
-	IMAGE img;
-	int kind;
-	int num;
-	int x, y;
-	void init(int i);
-	void update();
-	void show();
+public://variables
+	IMAGE img;//the image of the kind of fish
+	int kind;//the kind of fish of the system
+	int num;//the number of the kind of fish which have been caught
+	int x, y;//the location of the system
+public://functions
+	void init(int i);//init the fish-collection system
+	void update();//update the fish-collection system
+	void show();//show the fish-collection system
 };
